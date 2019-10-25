310263c40ab3acc1632fac9461f8aeec797b35b5
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-10-25 17:14:54 -0600

    initial commit

---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	
+	private int id;
+	private String title;
+	private String description;
+	private Integer releaseYear;
+	private int languageId;
+	private int rentalDuration;
+	private int length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	
+	
+}
